fd9b7866c70fc528520a02615e4740603a033ca5
GenevievePlantin <genevieveplantin7@gmail.com>
2017-10-01 19:30:16 -0400

    Start of Simon's Game
    Enhance desing with Element-UI and Font-Awesome

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <header class="padding-20 bg bg-blue">
+      Mini Games!
+    </header>
+    <el-row  :gutter="20">
+      <el-col :span="8">
+        <el-menu class="el-menu-vertical-demo" router="true">
+          <el-menu-item index="home">
+            <i class="fa fa-home"></i>
+            <template slot="title">Home</template>
+          </el-menu-item>
+          <el-menu-item index="simon">
+            <i class="el-icon-menu"></i>
+            <template slot="title">Simon</template>
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :span="16">
+        <router-view></router-view>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -12,12 +30,66 @@ export default {
 </script>
 
 <style>
+* {
+  padding:0px;
+  margin:0px;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  font-family: roboto, sans-serif;
+}
+
+header {
+  font-size: 30px;
+  font-weight: bold;
+  font-family: 'Kelly Slab', cursive;
+}
+
+.padding-20 {
+  padding:20px;
+}
+
+.bg {
+  color:#FFF;
+}
+
+.bg-blue {
+  background-color:#20A0FF;
+}
+
+.bg-red {
+  background-color:#FF4949;
+}
+
+.bg-yellow {
+  background-color:#F7BA2A;
+}
+
+.bg-green {
+  background-color:#13CE66;
+}
+
+.fa {
+  vertical-align: middle;
+  margin-right: 5px;
+  width: 24px;
+  text-align: center;
+  font-size: 20px;
+  top: -2px;
+  position: relative;
+}
+
+.el-icon-menu {
+  position: relative;
+  top: -1px;
+}
+
+.description {
+  padding:20px 0px;
+  font-size:18px;
 }
 </style>
---
+++ src/components/Simon.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="simon padding-20">
+    <h1>Let's play Simon !</h1>
+    <p class="description">When you are ready, the game will first show you a pattern of colors by lighting the buttons up. Then, at your turn, you will have to repeat the pattern. If you're wrong, you will start over. Good luck !</p>
+
+    <el-button type="primary" v-if="!isStarted" @click="startGame">Start</el-button>
+
+    <div v-if="isStarted">
+      <p class="legend">{{ simonsTurn }}</p>
+      <el-row :gutter="20">
+        <el-col :span="12"><div class="box bg bg-yellow"></div></el-col>
+        <el-col :span="12"><div class="box bg bg-blue"></div></el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="12"><div class="box bg bg-red"></div></el-col>
+        <el-col :span="12"><div class="box bg bg-green"></div></el-col>
+      </el-row>
+    </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'simon',
+
+  data () {
+    return {
+      simonsTurn: "Simon's turn. Look carefully!",
+      yourTurn: "It's your turn, repeat the pattern :)",
+      isStarted: false
+    }
+  },
+
+  methods: {
+    startGame () {
+      this.isStarted = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .box {
+    height:300px;
+  }
+
+  .legend {
+    text-align: center;
+    padding: 20px 0px;
+  }
+</style>
